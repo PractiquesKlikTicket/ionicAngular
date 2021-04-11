d8b85ccad417fd2310f9f6a25511becff13f6a2e
EstebanJPG <57748989+EstebanJPG@users.noreply.github.com>
2021-04-11 18:58:52 +0200

    Sin envio

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DatosPropComponent} from '../datos-prop/datos-prop.component';
 
+
+
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -17,30 +21,61 @@ export class FormularioComponent implements OnInit {
    
   };
 
-  
-  
-  formInput="";
-  formSelect="";
-  formCheck="";
+
 
   @ViewChild(DatosPropComponent) child;
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {}
 
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
   
   logForm() {
     console.log(this.form)
-    console.log(this.formSelect)
 
+
+    document.getElementById('enviarForm').addEventListener('click', (event) => {
+      event.preventDefault();
+
+   if(!this.form.input || this.form.input.length<=3){
+
+    let messageI="Introdice bien el input"
+
+    this.presentToast(messageI);
+    return;
+
+   }if(!this.form.checkbox){
+
+    let messageC="Acepta el Checbox"
+    this.presentToast(messageC);
+    return;
+
+   }if(!this.form.select){
+
+    let messageS="Selecciona una opcion"
+    this.presentToast(messageS);
+    return;
+
+   }
+
+    
+//navigate 
+
+
+
+
+    })
     
   }
 
-  checkBoxCheck1(){
-
-    this.form.checkbox=true;
-  }
+  
 
   checkBoxCheck(){
     console.log("cambio")
